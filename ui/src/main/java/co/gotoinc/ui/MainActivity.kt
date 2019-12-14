00229ae71b0a6986b6c6ce410e3100fac4d7f3e4
Denis Parvadov <den.parvadov@gmail.com>
2019-12-14 10:50:35 +0200

    start clean arch

---
+++ ui/src/main/java/co/gotoinc/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package co.gotoinc.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isInvisible
+import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.merge_toolbar.*
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Navigation.findNavController(this, R.id.fragmentContainer)
+            .addOnDestinationChangedListener { controller, destination, arguments ->
+                updateToolbar(destination.id)
+            }
+    }
+
+    private fun updateToolbar(destinationId: Int) {
+        when (destinationId) {
+            R.id.bookFragment -> {
+                backButtonToolbar.isInvisible = true
+                titleTextViewToolbar.setText(R.string.book_your_flight)
+            }
+        }
+    }
+}
